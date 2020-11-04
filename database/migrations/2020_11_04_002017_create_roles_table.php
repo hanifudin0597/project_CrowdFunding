77397b11b7a39c5a74393b70dc37f5be0ebf71eb
Hanifudin Alfauzi <hanifudin0597@gmail.com>
2020-11-04 08:09:28 +0700

    migration & erd laravel finish

---
+++ database/migrations/2020_11_04_002017_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->uuid('id_role')->primary();
+            // $table->timestamps();
+            $table->uuid('id_user');
+
+            $table->foreign('id_user')->references('id_user')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
---
+++ database/migrations/2020_11_04_002107_create_otp_codes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtpCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otp_codes', function (Blueprint $table) {
+            $table->uuid('id_otp')->primary();
+            $table->uuid('id_user')->nullable();
+
+            $table->foreign('id_user')->references('id_user')->on('users')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otp_codes');
+    }
+}
